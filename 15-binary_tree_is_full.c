0fbe2b18d5f00876eb7f30df920c0fc8c200f0a0
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-08-30 09:47:35 +0200

    modify func to check is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -11,10 +11,12 @@ int binary_tree_is_full(const binary_tree_t *tree)
 {
 	if (tree == NULL)
 		return (0);
+	else
+		return (1);
 
 	if (tree->left != NULL && tree->right != NULL)
 		return (1);
-
-	return (0);
+	else
+		return (0);
 
 }
