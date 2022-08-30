6af26d0c29459c5166068132f0e96b5abea57a92
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-08-30 09:40:10 +0200

    modify function of check is full

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -9,7 +9,7 @@
 
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	if (tree == NULL || tree->parent == NULL)
+	if (tree == NULL)
 		return (0);
 
 	if (tree->left != NULL && tree->right != NULL)
