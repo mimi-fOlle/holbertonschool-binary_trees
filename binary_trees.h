480cc29543b18b06636ff8d302c61aeeb46d8265
Yeh-Hsin LEE <oglanshi@gmail.com>
2022-09-02 21:00:43 +0200

    add modified header file

--- binary_trees.h
+++ binary_trees.h
@@ -53,5 +53,7 @@ binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
 int level_match (int a, int b);
 int is_perfect(binary_tree_t *tree);
+void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int));
+void current_level(const binary_tree_t *root, size_t level, void (*func)(int));
 
 #endif
